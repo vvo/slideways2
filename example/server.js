16e98d41f0a873676bfc9aac8b1ce02b3b54dd4d
vvo <vincent.voyer@gmail.com>
2014-09-28 20:39:45 +0200

    insert-css, domify, brfs, running example
    - use substack/insert-css, in prepend mode
    - use domify instead of hyperglue (no need for hyperglue layer)
    - use brfs instead of custom js modules
    - make example easier to run
    - remove "opts.insertCss" feature. Since we prepend the css to the DOM,
    we are now a true "widget" with easy cascading styling

--- example/server.js
+++ example/server.js
@@ -3,3 +3,4 @@ var ecstatic = require('ecstatic')
 
 var server = http.createServer(ecstatic(__dirname + '/static'));
 server.listen(5001);
+console.log('Example server listening on http://localhost:5001');
--- static/html.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-  '<div class="slideways">',
-    '<div class="runner"></div>',
-    '<div class="turtle"></div>',
-  '</div>'
-].join('\n')
